e63b956071d1cf308cebf7898245476b633e251b
赵姗 <2969138926@qq.com>
2020-01-09 12:00:08 +0800

    第二次修改

--- a.js
+++ a.js
@@ -3,4 +3,20 @@ $(function(){
         background:'yellow',
         color:'red'
     })
+    $('input.btn').click(function(){
+        $('.test1').css({
+            background:'red',
+            color:'#fff',
+            width:'100px',
+            height:'100px',
+            'font-size':'24px'
+        })
+    })
+
+
+
+
+
+
+
 })
